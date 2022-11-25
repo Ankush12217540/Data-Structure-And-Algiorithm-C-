3361d4a81b63e9f5ec5e4fe4af9314087a483a10
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2022-11-25 18:56:15 +0530

    printing letter pattern using while loop
    printing given letter  pattern using while loop:
    A  A  A  A
    B  B  B  B
    C  C  C  C
    D  D  D  D

---
+++ 4j.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the number of rows: ";
+    cin>>n;
+    int i=1;
+     char ch='A';
+    while (i<=n)
+    {
+        int j=1;
+        while(j<=n)
+        {
+            cout<<ch <<" ";
+            j=j+1;
+        }
+        cout<<endl;
+        ch=ch+1;
+        i=i+1;
+    }
+    return 0;
+}
