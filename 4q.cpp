4ad7a878c53308f8cb32cf138ac4eded9a9dad33
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2022-11-25 20:17:03 +0530

    letter pattern problem using while loop:
    Printing letter pattern using while loop(for n=5):
    "E
    D E
    C D E
    B C D E
    A B C D E"

---
+++ 4q.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    int n;
+    cout<<"Enter the number of rows: ";
+    cin>>n;
+    int i=1;
+    while (i<=n)
+    {
+        int j=1;
+        char c='A'+n-i;
+        while (j<=i)
+        {
+            cout<<c<<" ";
+            c=c+1;
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+    
+    return 0;
+}
