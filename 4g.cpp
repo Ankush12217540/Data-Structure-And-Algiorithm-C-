8908d37467c1459856f72c416de934fc19c44b64
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2022-11-25 17:44:59 +0530

    Pattern Problem using while loop
    Printing given below pattern using while loop:
    1
    2  3
    4  5  6
    7  8  9  10

---
+++ 4g.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter any number: ";
+    cin>>n;
+    int i=1;
+    int sum=1;
+    while (i<=n)
+    {
+        int j=1;
+        while (j<=i)
+        {
+            cout<<sum;
+            sum=sum+1;
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;      
+    }
+    
+    return 0;
+}
