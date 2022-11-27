a318be479109a47d8d251241944d38f7871446fb
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2022-11-27 23:08:35 +0530

    Program to Usage of more bitwise Operator
    Implementing Bitwise Operator Which includes:
    Left Shift
    Right Shift
    Post Increment/Decrement
    Pre Decrement/Decrement

---
+++ 5a.cpp
@@ -0,0 +1,23 @@
+// MORE BITWISE OPERATOR
+#include <iostream>
+using namespace std;
+int main()
+{
+    // left shift and Right Shift: 
+cout<<"17>>1 is: "<<(17>>1)<<endl;
+cout<<"17>>2 is: "<<(17>>2)<<endl;
+cout<<"19<<1 is: "<<(19<<1)<<endl;
+cout<<"19<<1 is: "<<(19<<1)<<endl;
+cout<<"21<<2 is: "<<(21<<2)<<endl;
+// Post Increment/Decrement  And Pre Decerement/Increment
+int i=7;
+cout<<"(++i) is: "<<(++i)<<endl;
+// 8
+cout<<"(i++)"<<(i++)<<endl;
+//8, i=9
+cout<<"(i++) is : "<<(i++)<<endl;
+//9, i=8
+cout<<"(--i) is: "<<(--i)<<endl;
+//7, i=7
+return 0;
+   }
