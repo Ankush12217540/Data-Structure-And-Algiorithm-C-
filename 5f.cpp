439196e019f9212790d63f5925fd481015a0734f
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2022-11-28 23:23:15 +0530

    No. of 1 bits in a number:
    Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
    
    Note:
    
    Note that in some languages, such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type. It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
    In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3, the input represents the signed integer. -3.
     
    
    Example 1:
    
    Input: n = 00000000000000000000000000001011
    Output: 3
    Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
    Example 2:
    
    Input: n = 00000000000000000000000010000000
    Output: 1
    Explanation: The input binary string 00000000000000000000000010000000 has a total of one '1' bit.
    Example 3:
    
    Input: n = 11111111111111111111111111111101
    Output: 31
    Explanation: The input binary string 11111111111111111111111111111101 has a total of thirty one '1' bits.
     
    
    Constraints:
    
    The input must be a binary string of length 32.

---
+++ 5f.cpp
@@ -0,0 +1,17 @@
+// Coded using Leetcode editor:
+//        Program to no of 1 bits in a number 
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count=0;
+       while(n!=0){
+          //checking last bit
+           if(n&1){
+               count++;
+           }
+          n=n>>1;
+       }
+       return count;
+    }
+};
