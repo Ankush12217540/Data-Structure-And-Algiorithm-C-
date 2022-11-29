edf8ee296b128cd0892028e804ea36f021d22539
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2022-11-29 11:14:41 +0530

    Decimal to binary conversion
    Write a program to convert Binary number to Decimal form
    using bitwise operation.

---
+++ 5g.cpp
@@ -0,0 +1,20 @@
+//                              CONVERSION OF DECIMAL TO BINARY:
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "If Decimal Number= ";
+    cin >> n;
+    int ans = 0;
+    int i = 0;
+    while (n != 0)
+    {
+        int bit = n&1;
+        ans = (bit * pow(10,i)) + ans;
+        n = n >> 1;
+        i++;
+    }
+    cout << "It's Binary Form = " << ans << endl;
+}
