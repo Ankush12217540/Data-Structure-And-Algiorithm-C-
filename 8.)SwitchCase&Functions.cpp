a2064ef53b0bdba0768995050d076f77d51ca658
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2022-12-01 19:41:34 +0530

    Switch case simple example

---
+++ 8.)SwitchCase&Functions.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num=2;
+    cout<<endl;
+    switch(num){
+        case 1: cout<<"First"<<endl;
+        case 2: cout<<"Second"<<endl;
+        default: cout<<"It is default case"<<endl;
+    }
+    cout<<endl;
+return 0;
+}
