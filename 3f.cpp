e68cb0da400bfe13072cc1262b593d32cddb1bd3
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2022-12-03 03:43:57 +0530

    Program to check Prime Number

--- 3f.cpp
+++ 3f.cpp
@@ -4,14 +4,20 @@ int main(){
     int n;
     cout<<"Enter the number to check whether its Prime or Not: ";
     cin>>n;
-    int a=2;
+    int a=2,flag=0;
     while (a<n)
     {
        if(n % a==0){
-        cout<<"Not a Prime Number";
+        flag=1;
+           break;
        }
        a=a+1;
+    if(flag==0){
+        cout<<"It's a Prime Number;
+        }
+    else{
+        cout<<"Not a Prime Number";
     }
-    cout<<"It's a Prime Number";
-    return 0;
-}
+        return 0;
+    }
+      
