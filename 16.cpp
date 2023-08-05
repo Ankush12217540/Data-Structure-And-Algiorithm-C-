3750ca50d9dad5bb64beaf01fc240f4a2ae4b375
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2023-08-05 23:35:00 +0530

    Add files via upload

---
+++ 16.cpp
@@ -0,0 +1,23 @@
+// Selection Sorting
+// it has different round || passses
+// in each round , select smallest element and usko right jagah par place karna hain
+
+# include<iostream> 
+using namespace std;
+
+void selectionSort(vector<int> &arr, int n){
+    for(int i=0; i<n-1; i++){
+        int minIndex=i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j]< arr[mainIndex]){
+            minIndex=j;
+            };
+        };
+        swap(arr[minIndex], arr[i]);
+    };
+}
+
+int main(){
+    int arr[5]={2,3,1,4,6};
+    cout<<"The Selection sort of the array: "<<arr<<" is: ";
+}
