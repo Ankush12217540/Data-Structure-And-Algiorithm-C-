f169032c6dce2012a30bb7f55abe98d4f2cc6059
Ankush Kumar <115498007+Ankush12217540@users.noreply.github.com>
2023-08-09 23:52:11 +0530

    Add files via upload

---
+++ 19e.cpp
@@ -0,0 +1,21 @@
+//                   ----QUEUE----\
+// first in first out.. principle
+
+
+#include<iostream>
+#include<queue>
+
+using namespace std;
+int main(){
+    queue<string> q;
+    q.push("Love");
+    q.push("Babbar");
+    q.push("Kumar");
+
+    cout<<"First Element: "<<q.front()<<endl;
+    q.pop();
+    cout<<"First Element: "<<q.front()<<endl;
+
+    cout<<"Size after pop: "<<q.size()<<endl;
+
+}
